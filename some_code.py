65529639e0202a7ed45c7f996d20de090daf92fd
samholdawayinshur <163404220+samholdawayinshur@users.noreply.github.com>
2025-02-10 16:18:09 +0000

    add a dummy aws secret

--- some_code.py
+++ some_code.py
@@ -4,3 +4,6 @@ print('blah blah')
 uhoh = "password123"
 
 print(f"I am not very clever so I will print my password: {uhoh}")
+
+# Nicked this from the gitleaksignore file in the gitleaks repo
+aws_secret = "AKIAIMNOJVGFDXXXE4OA"
